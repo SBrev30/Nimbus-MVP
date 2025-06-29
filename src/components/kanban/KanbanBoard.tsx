5479a2102ecf41fa19aecacb5306b97363440f8d
SBrev30 <sheldonbrev@hotmail.com>
2025-06-29 02:52:41 -0400

    Updated KanbanBoard.tsx

--- src/components/kanban/KanbanBoard.tsx
+++ src/components/kanban/KanbanBoard.tsx
@@ -128,11 +128,7 @@ export function KanbanBoard({ projectId, projectTitle, onBackToLibrary }: Kanban
                 Add Task
               </button>
               
-              {/* Share button */}
-              <button className="p-2 text-gray-500 hover:text-gray-700 hover:bg-gray-100 rounded-lg transition-colors">
-                <Share2 className="w-5 h-5" />
-              </button>
-              
+             
               {/* More options */}
               <button className="p-2 text-gray-500 hover:text-gray-700 hover:bg-gray-100 rounded-lg transition-colors">
                 <MoreHorizontal className="w-5 h-5" />
