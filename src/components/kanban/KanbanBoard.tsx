bece5e6081627713157d35341d8576bc160b9b12
SBrev30 <sheldonbrev@hotmail.com>
2025-06-29 02:56:48 -0400

    Updated KanbanBoard.tsx

--- src/components/kanban/KanbanBoard.tsx
+++ src/components/kanban/KanbanBoard.tsx
@@ -161,12 +161,6 @@ export function KanbanBoard({ projectId, projectTitle, onBackToLibrary }: Kanban
               </button>
             </div>
 
-            <div className="flex items-center gap-4">
-              <button className="text-gray-500 hover:text-gray-700">
-                <Share2 className="w-4 h-4" />
-                <span className="ml-1 text-sm font-medium">Share</span>
-              </button>
-            </div>
           </div>
         </div>
       </div>
