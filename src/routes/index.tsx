90ae3b203e5750ef22cf1473eb7839a69ae28f19
SBrev30 <sheldonbrev@hotmail.com>
2025-06-30 09:52:31 -0400

    Create index.tsx

---
+++ src/routes/index.tsx
@@ -0,0 +1,6 @@
+// This automatically becomes your "/" route
+import NimbusLandingPage from '../components/nimbus-landing-page'
+
+export default function HomePage() {
+  return <NimbusLandingPage />
+}
